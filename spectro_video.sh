a2f35951da397c4a090a2feecf1fa1a6af2d5afe
lucasvoirin <lucas.voirin@mailo.com>
2024-02-14 22:55:43 -0500

    adding variables

--- spectro_video.sh
+++ spectro_video.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Define variables
+frame_rate=35
+spectro_height=600
+spectro_width=600
+
 # Check if the argument is provided
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 input.wav"  # Provide usage message if argument count is incorrect
+    echo "Usage: bash $0 input.wav"  # Provide usage message if argument count is incorrect
     exit 1  # Exit script with error status
 fi
 
@@ -34,7 +39,6 @@ sox $input_file -n channels && sox $input_file mono.wav remix 1
 
 sample_rate=$(sox --i -r mono.wav)
 
-
 # Add 2.5 seconds of silence before and after
 sox -n -r $sample_rate silence.wav trim 0.0 2.5
 sox mono.wav silence.wav mono_silence.wav
@@ -47,7 +51,6 @@ mkdir -p frames
 total_duration=$(soxi -D mono.wav | cut -d '.' -f 1)
 
 # Determine total number of frames needed
-frame_rate=50
 frame_count=$((frame_rate * total_duration))
 
 # Loop through frames
@@ -56,7 +59,7 @@ for ((i=0; i<frame_count; i++)); do
 
     # Generate spectrogram for the frame
     frame_file="frames/frame$(printf %03d $i).png"
-    sox silence_mono_silence.wav -n trim $start_time 5 spectrogram -x 500 -y 500 -r -o "$frame_file"
+    sox silence_mono_silence.wav -n trim $start_time 5 spectrogram -x $spectro_width -y $spectro_height -r -o "$frame_file"
 
 percentage=$(( (i * 100) / frame_count + 1 ))
 printf "\rProgress: %s %% " "$percentage"  # Print progress as percentage on the same line
@@ -66,10 +69,10 @@ done
 echo
 
 # Use ffmpeg to create the video
-ffmpeg -framerate $frame_rate -i frames/frame%03d.png -i mono.wav -c:v libx264 -preset slow -crf 20 -c:a aac -b:a 160k -vf "drawbox=x=250:y=10:w=1:h=480:color=red,format=yuv420p" -movflags +faststart "$file_name.mp4" > /dev/null 2>&1
+ffmpeg -framerate $frame_rate -i frames/frame%03d.png -i mono.wav -c:v libx264 -preset slow -crf 20 -c:a aac -b:a 160k -vf "drawbox=x=$((spectro_width/2)):y=10:w=1:h=$((spectro_heigt-20)):color=red,format=yuv420p" -movflags +faststart "$file_name.mp4" > /dev/null 2>&1
 
 cp "$file_name.mp4" ..  # Copy the generated video to the parent directory
 
 cd ..  # Move back to the parent directory
 
-# rm -r "$file_name-temp"  # Remove the temporary directory and its contents
+rm -r "$file_name-temp"  # Remove the temporary directory and its contents
